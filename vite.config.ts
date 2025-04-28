54c442995578db2651dbaba3804bf995dc3e1987
aaloksg <56730032+aaloksg@users.noreply.github.com>
2025-04-28 22:26:14 +0200

    tries to solve router issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,26 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
     base: '/',
-    plugins: [vue(), vueDevTools(), tailwindcss()],
+    plugins: [
+        vue(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'dist/index.html', // copy after build
+                    dest: '', // place it in dist/ root
+                    rename: '404.html', // rename it
+                },
+            ],
+            hook: 'writeBundle', // ensures it copies after build finishes
+        }),
+        vueDevTools(),
+        tailwindcss(),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
